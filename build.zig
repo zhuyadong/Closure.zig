bf322b1f91db22e44415d9a9a37daa06b293bc8b
zhuyadong <lordix@gmail.com>
2024-07-05 13:19:46 +0800

    export module

--- build.zig
+++ build.zig
@@ -1,17 +1,14 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "Closure",
+    const closure = b.createModule(.{
         .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
     });
 
-    b.installArtifact(lib);
+    try b.modules.put(b.dupe("Closure"), closure);
 
     const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
